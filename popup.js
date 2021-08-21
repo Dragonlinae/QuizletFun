7a0affc3f920ba3f63ad8bdfe205d57c5410c66d
Joshua <joshuahsu108@gmail.com>
2021-08-20 17:13:12 -0700

    1.0.5 css time

--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
-console.log("nom")
-
 document.addEventListener('DOMContentLoaded', function () {
     var btn = document.getElementById("go");
     btn.addEventListener("click", nomCookie);
 });
 
-function nomCookie(){
+async function nomCookie(){
+
     chrome.cookies.getAll({domain: "quizlet.com"}, function(cookies) {
         for(var i=0; i<cookies.length;i++) {
             chrome.cookies.remove({url: "https://quizlet.com" + cookies[i].path, name: cookies[i].name});
         }
+        // TODO: Refresh page if refresh page toggle is toggled
+        //chrome.extension.getBackgroundPage().window.location.reload(true)
     });
 }
