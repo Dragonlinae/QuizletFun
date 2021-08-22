310ca5777a0507030baa30b65c41ac3c9b78bf12
Joshua <joshuahsu108@gmail.com>
2021-08-21 17:25:10 -0700

    1.0.6 Cookie counter and page refresher

--- popup.js
+++ popup.js
@@ -3,13 +3,34 @@ document.addEventListener('DOMContentLoaded', function () {
     btn.addEventListener("click", nomCookie);
 });
 
+nommedCookies = 0;
+chrome.storage.local.get("nommies", function(noms){
+    if (noms.nommies == undefined){
+        noms.nommies = 0;
+    }
+    document.getElementsByClassName("cookiecounter")[0].innerHTML = noms.nommies;
+});
 async function nomCookie(){
+
 
     chrome.cookies.getAll({domain: "quizlet.com"}, function(cookies) {
         for(var i=0; i<cookies.length;i++) {
             chrome.cookies.remove({url: "https://quizlet.com" + cookies[i].path, name: cookies[i].name});
         }
-        // TODO: Refresh page if refresh page toggle is toggled
-        //chrome.extension.getBackgroundPage().window.location.reload(true)
+
+        chrome.storage.local.get("nommies", function(noms){
+            if (noms.nommies == undefined){
+                noms.nommies = 0;
+            }
+            nommedCookies = noms.nommies + cookies.length;
+            document.getElementsByClassName("cookiecounter")[0].innerHTML = nommedCookies;
+            chrome.storage.local.set({ "nommies": nommedCookies }, function(){
+                if (document.getElementById("toggle1").checked) {
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                        chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
+                    });
+                }
+            }); 
+        });
     });
 }
